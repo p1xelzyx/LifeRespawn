849eeaded83e1affc0354308bdb8c6126c77c30b
p1xelzyx <65184909+p1xelzyx@users.noreply.github.com>
2025-05-07 08:20:21 +0200

    log section

--- svelte-liferespawn/src/routes/app/dashboard/+page.svelte
+++ svelte-liferespawn/src/routes/app/dashboard/+page.svelte
@@ -1,12 +1,34 @@
 <script></script>
 
-
-<section class="app-section">
-    lallalal
+<section class="app-section section-1">
+    <div class="log-info">
+        <p>Write down how you feel right now, or what you just did.</p>
+        <p>You can always log your actions at the end of the day down below.</p>
+    </div>
+    <div class="log-button">
+        <button>Log mood</button>
+        <button>Log action</button>
+    </div>
 </section>
 
-
-
 <style>
-        
+    .log-info p {
+        font-size: 1.1em;
+        margin-bottom: 15px;
+    }
+    .section-1 {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .log-button button {
+        margin: 40px;
+        font-size: 1.3em;
+        color: white;
+        width: 150px;
+        height: 150px;
+        padding: 30px;
+        background-color: var(--main-color);
+        border-radius: 50%;
+    }
 </style>
