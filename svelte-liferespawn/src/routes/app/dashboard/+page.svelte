2120325d3041c0537fdd32fc8ac32d2c6f0f1ebd
p1xelzyx <65184909+p1xelzyx@users.noreply.github.com>
2025-05-07 08:36:31 +0200

    style

--- svelte-liferespawn/src/routes/app/dashboard/+page.svelte
+++ svelte-liferespawn/src/routes/app/dashboard/+page.svelte
@@ -5,7 +5,7 @@
         <p>Write down how you feel right now, or what you just did.</p>
         <p>You can always log your actions at the end of the day down below.</p>
     </div>
-    <div class="log-button">
+    <div class="log-buttons">
         <button>Log mood</button>
         <button>Log action</button>
     </div>
@@ -21,7 +21,7 @@
         flex-direction: row;
         justify-content: space-between;
     }
-    .log-button button {
+    .log-buttons button {
         margin: 40px;
         font-size: 1.3em;
         color: white;
@@ -30,5 +30,15 @@
         padding: 30px;
         background-color: var(--main-color);
         border-radius: 50%;
+        box-shadow: 0px 8px 0px 0px rgba(82, 100, 202, 0.6);
+        transition: all 0.1s;
+    }
+    .log-buttons button:hover {
+        box-shadow: 0px 5px 0px 0px rgba(82, 100, 202, 0.6);
+        transform: translateY(3px);
+    }
+    .log-buttons button:active {
+        box-shadow: 0px 3px 0px 0px rgba(82, 100, 202, 0.6);
+        transform: translateY(5px);
     }
 </style>
