dccfa0096758c728ca171e9687f5903a2c798c31
p1xelzyx <65184909+p1xelzyx@users.noreply.github.com>
2025-05-12 17:59:38 +0200

    action list

--- svelte-liferespawn/src/routes/app/actions/+page.svelte
+++ svelte-liferespawn/src/routes/app/actions/+page.svelte
@@ -1,5 +1,101 @@
 <script>
-
+    const impactLevels = [
+        {
+            name: "🔴 Harmful",
+            description:
+                "This action actively sets you back. It drains energy, causes regret, or reinforces bad habits.",
+        },
+        {
+            name: "🟠 Unhelpful",
+            description:
+                "Time spent here doesn’t contribute to your goals or well-being. Often feels like a waste.",
+        },
+        {
+            name: "🟡 Neutral",
+            description:
+                "Doesn’t help or hurt. Just exists. Sometimes necessary, but not meaningful.",
+        },
+        {
+            name: "🟢 Mildly Beneficial",
+            description:
+                "A small step in the right direction. Feels good, supports your growth a little.",
+        },
+        {
+            name: "✅ Productive",
+            description:
+                "Clearly contributes to your goals. Makes you feel better or closer to who you want to be.",
+        },
+        {
+            name: "💪 Strongly Beneficial",
+            description:
+                "Excellent for long-term growth. Physically, mentally, or emotionally rewarding.",
+        },
+        {
+            name: "🌟 Core Identity",
+            description:
+                "This defines the person you’re trying to become. Deeply aligned with your ideal self.",
+        },
+    ];
 </script>
 
-<h1>Your actions</h1>
+<section class="app-section">
+    <div class="top">
+        <h1>Your actions</h1>
+        <button class="new-action">New action</button>
+    </div>
+    <div class="action-list">
+        {#each ". ".repeat(10).split(".") as nc}
+            <button class="action">
+                <h2>Clean room</h2>
+                <p>🟢 Mildly Beneficial</p>
+            </button>
+        {/each}
+    </div>
+</section>
+
+<style>
+    h1 {
+        font-size: 2em;
+    }
+
+    .top {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 60px;
+    }
+
+    .action-list {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 30px;
+    }
+
+    .action {
+        background-color: var(--bg-color);
+        color: white;
+        padding: 20px;
+        border-radius: 11px;
+        transition: 0.1s all;
+    }
+
+    .action:hover {
+        background-color: var(--main-color);
+        cursor: pointer;
+    }
+
+    .new-action {
+        font-size: 1.5em;
+        color: var(--main-color);
+        background-color: transparent;
+        padding: 15px;
+        border-radius: 15px;
+        border: 2px solid var(--main-color);
+        transition: 0.1s all;
+    }
+
+    .new-action:hover {
+        color: white;
+        background-color: var(--main-color);
+        cursor: pointer;
+    }
+</style>
